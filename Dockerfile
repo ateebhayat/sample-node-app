f59c0100c3c355aa134b64d47784a25e2a7a7484
ateebhayat <20042147@mydbs.ie>
2024-11-08 21:58:59 +0500

    chore: updated docker file

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,19 @@
 FROM node
+
+# Set the working directory in the container
 WORKDIR /app
 
-COPY . .
-EXPOSE 3000
-
+# Copy the package.json and package-lock.json files first to leverage Docker caching
 COPY package*.json ./
 
-ENTRYPOINT start npm
+# Install dependencies
+RUN npm install
+
+# Copy the rest of the application files
+COPY . .
+
+# Expose the app's port
+EXPOSE 3000
+
+# Set the entry point command to start the application
+CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -4,50 +4,47 @@ const request = require('request');
 const wikip = require('wiki-infobox-parser');
 
 //ejs
-app.set("view engine", 'ejs');
+app.set('view engine', 'ejs');
 
 //routes
-app.get('/', (req,res) =>{
-    res.render('index');
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.get('/index', (req,response) =>{
-    let url = "https://en.wikipedia.org/w/api.php"
-    let params = {
-        action: "opensearch",
-        search: req.query.person,
-        limit: "1",
-        namespace: "0",
-        format: "json"
+app.get('/index', (req, response) => {
+  let url = 'https://en.wikipedia.org/w/api.php';
+  let params = {
+    action: 'opensearch',
+    search: req.query.person,
+    limit: '1',
+    namespace: '0',
+    format: 'json',
+  };
+
+  url = url + '?';
+  Object.keys(params).forEach((key) => {
+    url += '&' + key + '=' + params[key];
+  });
+  console.log('test user');
+  //get wikip search string
+  request(url, (err, res, body) => {
+    if (err) {
+      response.redirect('404');
     }
-
-    url = url + "?"
-    Object.keys(params).forEach( (key) => {
-        url += '&' + key + '=' + params[key]; 
+    result = JSON.parse(body);
+    x = result[3][0];
+    x = x.substring(30, x.length);
+    //get wikip json
+    wikip(x, (err, final) => {
+      if (err) {
+        response.redirect('404');
+      } else {
+        const answers = final;
+        response.send(answers);
+      }
     });
-
-    //get wikip search string
-    request(url,(err,res, body) =>{
-        if(err) {
-            response.redirect('404');
-        }
-            result = JSON.parse(body);
-            x = result[3][0];
-            x = x.substring(30, x.length); 
-            //get wikip json
-            wikip(x , (err, final) => {
-                if (err){
-                    response.redirect('404');
-                }
-                else{
-                    const answers = final;
-                    response.send(answers);
-                }
-            });
-    });
-
-    
+  });
 });
 
 //port
-app.listen(3000, console.log("Listening at port 3000..."))
+app.listen(3000, console.log('Listening at port 3000...'));
